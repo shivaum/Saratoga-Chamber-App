b132f9a3359f4501266b9282abdc011ed6c423d6
Shivaum Kumar <shivaum1@gmail.com>
2016-08-01 17:47:32 -0700

    Initial Commit

---
+++ Saratoga Chamber App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Saratoga Chamber App
+//
+//  Created by Shivaum Kumar on 8/1/16.
+//  Copyright © 2016 Shivaum Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
